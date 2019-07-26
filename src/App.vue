8977c2ddce983618480218edd790fa5257b17022
Niels Mentink <niels.mentink@outlook.com>
2019-07-26 18:37:11 +0200

    Added topbar with icon

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from "./components/TopBar";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    TopBar
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// @import "./assets/styles/main.scss";
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <header>
+    <div class="headercontent">
+      <img class="logo" alt="{...}" src="../assets/Icon.svg" />
+      <h1 class="title">Niels Mentink</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TopBar"
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 64px;
+  background-color: #4030e8;
+  color: #fff;
+  display: grid;
+  grid-template-columns: auto 100% auto;
+  grid-template-areas: "header header header";
+
+  @media screen {
+    grid-template-columns: auto 1170px auto;
+    grid-template-areas: ". header .";
+  }
+
+  .headercontent {
+    grid-area: header;
+
+    .logo {
+      height: 34px;
+      width: 34px;
+      margin: 15px;
+      float: left;
+      display: block;
+    }
+
+    .title {
+      font-family: "Roboto Mono", monospace;
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 64px;
+      margin: 0;
+      float: left;
+      display: block;
+    }
+  }
+}
+</style>
