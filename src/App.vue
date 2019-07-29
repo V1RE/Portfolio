f9e25794dfe8fa840eaa6cbbad4076d7b2519c16
Niels Mentink <niels.mentink@outlook.com>
2019-07-29 13:19:52 +0200

    Started working on a layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
     <TopBar />
-    <MaterialCard>
-      test
-      <template v-slot:content>Dit is echt zieke content</template>
-    </MaterialCard>
+    <MainLayout>
+      <MaterialCard>
+        Hallo
+        <template v-slot:content>Dit is echt zieke content</template>
+      </MaterialCard>
+      <MaterialCard>
+        te
+        <br />st
+        <template v-slot:content>Dit is echt zieke content</template>
+      </MaterialCard>
+      <MaterialCard>
+        wederom een leuke test hoor
+        <template v-slot:content>Dit is echt zieke content</template>
+      </MaterialCard>
+    </MainLayout>
   </div>
 </template>
 
 <script>
 import TopBar from "./components/TopBar";
 import MaterialCard from "./components/MaterialCard";
+import MainLayout from "./components/MainLayout";
 
 export default {
   name: "app",
   components: {
     TopBar,
-    MaterialCard
+    MaterialCard,
+    MainLayout
   }
 };
 </script>
 
 <style lang="scss">
-.materialcard {
-  top: 100px;
-  position: absolute;
+body {
+  margin: 0;
 }
 </style>
--- src/components/MaterialCard.vue
+++ src/components/MaterialCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="materialcard" v-on:click="full = !full">
+  <div class="materialcard" v-on:click="full = !full" :class="{fullscreen: full}">
     <div class="excerpt">
       <slot></slot>
     </div>
@@ -25,13 +25,47 @@ export default {
   background-color: #fff;
   font-family: "Roboto Mono", monospace;
   color: #000;
+  float: left;
+  width: calc(50% - 60px);
+  max-width: 1170px;
   padding: 15px;
+  margin: 0 15px 30px 15px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: box-shadow 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 
   &:hover {
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
     cursor: pointer;
+  }
+
+  &.fullscreen {
+    margin: 0;
+    height: calc(100vh - 94px);
+    width: calc(100vw - 30px);
+    position: absolute;
+    top: 64px;
+    left: 0;
+  }
+
+  .content {
+    animation: fade 0.8s ease 0.5s forwards;
+    opacity: 0;
+    max-width: 0;
+    max-height: 0;
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 0;
+      max-width: initial;
+      max-height: initial;
+    }
+
+    to {
+      opacity: 1;
+      max-width: initial;
+      max-height: initial;
+    }
   }
 }
 </style>
