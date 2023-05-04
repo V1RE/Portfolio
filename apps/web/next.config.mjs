0308badc0f9726afd44a9e76b6890d73f741d634
Niels Mentink <niels@ment.ink>
2023-05-05 01:12:08 +0200

    🚧 WIP

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,23 +1,13 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
-import { withSentryConfig } from '@sentry/nextjs';
-
-const shouldAnalyze = process.env.ANALYZE === 'true';
-
-/** @type {(config?: import('next').NextConfig) => import('next').NextConfig} */
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: shouldAnalyze,
-});
-
 /** @type {import('next').NextConfig} */
 const config = {
+  experimental: {
+    appDir: true,
+    typedRoutes: true,
+  },
   reactStrictMode: true,
 
   /** We run eslint as a separate task in CI */
   eslint: { ignoreDuringBuilds: true },
 };
 
-export default withSentryConfig(
-  shouldAnalyze ? withBundleAnalyzer(config) : config,
-  {},
-  { hideSourceMaps: true },
-);
+export default config;
---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,25 @@
+import resolveConfig from 'tailwindcss/resolveConfig';
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+const config = resolveConfig({
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-manrope)', ...fontFamily.sans],
+        mono: ['var(--font-roboto-mono)', ...fontFamily.mono],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+});
+
+export default config;
